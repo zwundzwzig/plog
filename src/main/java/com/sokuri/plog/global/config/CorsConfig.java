e942056bdcdff8bfc527884ce66b64d81456c561
zwundzwzig <amoeba8282@gmail.com>
2024-01-11 11:19:31 +0900

    [refactor] cors error

--- src/main/java/com/sokuri/plog/global/config/CorsConfig.java
+++ src/main/java/com/sokuri/plog/global/config/CorsConfig.java
@@ -1,25 +1,27 @@
 package com.sokuri.plog.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 @Configuration
 public class CorsConfig {
+  @Value("${sokuri.address}")
+  private String address;
+  @Value("${server.port}")
+  private Integer port;
+
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedHeaders(List.of("Authorization"));
-    configuration.addAllowedOrigin("http://13.209.208.58:8080");
-    configuration.addAllowedOrigin("http://13.209.208.58:3000");
-    configuration.addAllowedOrigin("http://13.209.208.58:8080");
+    configuration.addAllowedOrigin(address + ":" + port);
+    configuration.addAllowedOrigin("http://localhost:3000");
 
     configuration.setAllowCredentials(true);
     configuration.setAllowedHeaders(
